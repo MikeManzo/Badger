60d569f0d7a4e3fabb56201ca9f1abeb117051f7
Mike Manzo <manzo.mike@gmail.com>
2020-04-07 14:22:39 -0400

    Initial Commit

---
+++ Badger/Badger.h
@@ -0,0 +1,19 @@
+//
+//  Badger.h
+//  Badger
+//
+//  Created by Mike Manzo on 4/7/20.
+//  Copyright © 2020 Mike Manzo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Badger.
+FOUNDATION_EXPORT double BadgerVersionNumber;
+
+//! Project version string for Badger.
+FOUNDATION_EXPORT const unsigned char BadgerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Badger/PublicHeader.h>
+
+
